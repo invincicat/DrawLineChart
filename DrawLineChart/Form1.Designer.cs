0e3d9f81d587061ca05d747d205c49d5f4ed2da3
17826833409 <17826833409@163.com>
2023-01-11 12:18:00 +0800

    添加项目文件。

---
+++ DrawLineChart/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace DrawLineChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chtLine = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnLoadPath = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCurrentLine = new System.Windows.Forms.TextBox();
+            this.btnLastLine = new System.Windows.Forms.Button();
+            this.btnNextLine = new System.Windows.Forms.Button();
+            this.btnToExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chtLine)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chtLine
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chtLine.ChartAreas.Add(chartArea1);
+            this.chtLine.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chtLine.Legends.Add(legend1);
+            this.chtLine.Location = new System.Drawing.Point(0, 0);
+            this.chtLine.Name = "chtLine";
+            this.chtLine.Size = new System.Drawing.Size(872, 580);
+            this.chtLine.TabIndex = 1;
+            this.chtLine.Text = "LineChart";
+            this.chtLine.Click += new System.EventHandler(this.chtLine_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.chtLine);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(374, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(872, 580);
+            this.panel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.70376F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70.29623F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1249, 586);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnToExcel);
+            this.panel2.Controls.Add(this.btnLoadPath);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtCurrentLine);
+            this.panel2.Controls.Add(this.btnLastLine);
+            this.panel2.Controls.Add(this.btnNextLine);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(365, 580);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnLoadPath
+            // 
+            this.btnLoadPath.Location = new System.Drawing.Point(3, 371);
+            this.btnLoadPath.Name = "btnLoadPath";
+            this.btnLoadPath.Size = new System.Drawing.Size(116, 37);
+            this.btnLoadPath.TabIndex = 4;
+            this.btnLoadPath.Text = "加载文件路径";
+            this.btnLoadPath.UseVisualStyleBackColor = true;
+            this.btnLoadPath.Click += new System.EventHandler(this.btnLoadPath_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(3, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "当前行：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtCurrentLine
+            // 
+            this.txtCurrentLine.Location = new System.Drawing.Point(7, 231);
+            this.txtCurrentLine.Name = "txtCurrentLine";
+            this.txtCurrentLine.Size = new System.Drawing.Size(100, 25);
+            this.txtCurrentLine.TabIndex = 2;
+            this.txtCurrentLine.Text = "1";
+            this.txtCurrentLine.TextChanged += new System.EventHandler(this.txtCurrentLine_TextChanged);
+            // 
+            // btnLastLine
+            // 
+            this.btnLastLine.Location = new System.Drawing.Point(0, 82);
+            this.btnLastLine.Name = "btnLastLine";
+            this.btnLastLine.Size = new System.Drawing.Size(80, 54);
+            this.btnLastLine.TabIndex = 1;
+            this.btnLastLine.Text = "上一行";
+            this.btnLastLine.UseVisualStyleBackColor = true;
+            this.btnLastLine.Click += new System.EventHandler(this.btnLastLine_Click);
+            // 
+            // btnNextLine
+            // 
+            this.btnNextLine.Location = new System.Drawing.Point(0, 0);
+            this.btnNextLine.Name = "btnNextLine";
+            this.btnNextLine.Size = new System.Drawing.Size(80, 54);
+            this.btnNextLine.TabIndex = 0;
+            this.btnNextLine.Text = "下一行";
+            this.btnNextLine.UseVisualStyleBackColor = true;
+            this.btnNextLine.Click += new System.EventHandler(this.btnNextLine_Click);
+            // 
+            // btnToExcel
+            // 
+            this.btnToExcel.Location = new System.Drawing.Point(7, 448);
+            this.btnToExcel.Name = "btnToExcel";
+            this.btnToExcel.Size = new System.Drawing.Size(100, 37);
+            this.btnToExcel.TabIndex = 4;
+            this.btnToExcel.Text = "导入excel";
+            this.btnToExcel.UseVisualStyleBackColor = true;
+            this.btnToExcel.Click += new System.EventHandler(this.btnToExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1249, 586);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chtLine)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtLine;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCurrentLine;
+        private System.Windows.Forms.Button btnLastLine;
+        private System.Windows.Forms.Button btnNextLine;
+        private System.Windows.Forms.Button btnLoadPath;
+        private System.Windows.Forms.Button btnToExcel;
+    }
+}
+
---
+++ DrawLineChart/Form1.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace DrawLineChart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStrat_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+
+        private void chtLine_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public static int Line = 1;
+        public static string strPath = @"C:\123.txt";
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            chtLine.Series.Add("X坐标");
+            chtLine.Series.Add("Y坐标");
+
+            ShowLine();
+
+            //string[] strs2 = File.ReadAllLines(strPath);
+            //// 也可以指定编码方式 
+            ////string[] strs2 = File.ReadAllLines(@"C:\Users\SK-ED\Desktop\123.txt", Encoding.ASCII);
+
+            //int n = strs2.Length;
+
+            //////清空表格中的数据
+            ////foreach (var series in chtLine.Series)
+            ////{
+            ////    series.Points.Clear();
+            ////    //series.DeleteCustomProperty("0-Xa");
+            ////    //series.;
+
+            ////}
+
+
+            //for (int i = 0; i < 1; i++)
+            //{
+            //    string tempA = strs2[i + Form1.Line - 1];
+
+            //    string[] vs = tempA.Split('T');
+            //    string tempD = vs[1];
+
+            //    string tempB = tempD.Remove(0, 8);
+            //    string[] tempC = tempB.Split(';');
+
+            //    double[] tempNumD = new double[72];
+
+            //    //字符串类型数组转double类型数组
+            //    for (int m = 0; m < tempC.Length - 1; m++)
+            //    {
+            //        tempNumD[m] = double.Parse(tempC[m]);
+
+            //    }
+
+            //    double[] X = new double[36];
+            //    double[] Y = new double[36];
+            //    for (int q = 0; q < 36; q++)
+            //    {
+
+            //        X[q] = tempNumD[2 * q];
+            //        Y[q] = tempNumD[2 * q + 1];
+            //    }
+
+
+
+            //    double[] diffXa = new double[36];
+            //    double[] diffXb = new double[18];
+
+            //    double[] diffYa = new double[36];
+            //    double[] diffYb = new double[18];
+
+            //    for (int k = 0; k < 18; k++)
+            //    {
+            //        diffXa[k] = X[k] - X[0] + k * 32.2;
+            //        diffXa[k+18] = X[k + 18] - X[18] + k * 32.2;
+
+            //        diffYa[k] = Y[k] - Y[0];
+            //        diffYa[k+18] = Y[k + 18] - Y[18];
+
+
+            //    }
+
+
+
+
+
+
+            //    double[] xData = {  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36 };
+
+
+            //    chtLine.ChartAreas[0].AxisX.Interval = 1;
+
+            //    chtLine.Series.Add(i.ToString() + "-Xa");
+            //    chtLine.Series[4 * i].ChartType = SeriesChartType.Line;//设置为折线
+            //    chtLine.Series[4 * i].Points.DataBindXY(xData, diffXa);
+            //    chtLine.Series[4 * i].BorderWidth = 1; //折线粗细
+            //    chtLine.Series[4 * i].Color = System.Drawing.Color.Red;//设置折线颜色                                         
+
+            //    //chtLine.Series.Add(i.ToString() + "-Xb");
+            //    //chtLine.Series[4 * i + 1].ChartType = SeriesChartType.Line;
+            //    //chtLine.Series[4 * i + 1].Points.DataBindXY(xData, diffXb);
+            //    //chtLine.Series[4 * i + 1].BorderWidth = 1;
+            //    //chtLine.Series[4 * i + 1].Color = System.Drawing.Color.Black;//设置折线颜色                                         
+
+
+
+            //    chtLine.Series.Add(i.ToString() + "-Ya");
+            //    chtLine.Series[4 * i + 1].ChartType = SeriesChartType.Line;
+            //    chtLine.Series[4 * i + 1].Points.DataBindXY(xData, diffYa);
+            //    chtLine.Series[4 * i + 1].BorderWidth = 1;
+            //    chtLine.Series[4 * i + 1].Color = System.Drawing.Color.Blue;//设置折线颜色                                         
+
+
+
+            //    //chtLine.Series.Add(i.ToString() + "-Yb");
+            //    //chtLine.Series[4 * i + 3].ChartType = SeriesChartType.Line;
+            //    //chtLine.Series[4 * i + 3].Points.DataBindXY(xData, diffYb);
+            //    //chtLine.Series[4 * i + 3].BorderWidth = 1;
+            //    //chtLine.Series[4 * i + 3].Color = System.Drawing.Color.Green;//设置折线颜色                                         
+
+
+
+
+
+        }
+
+        //}
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLastLine_Click(object sender, EventArgs e)
+        {
+            Form1.Line--;
+            ShowLine();
+            txtCurrentLine.Text = ((Form1.Line)).ToString();
+        }
+
+        private void btnNextLine_Click(object sender, EventArgs e)
+        {
+            Form1.Line++;
+            ShowLine();
+            txtCurrentLine.Text = ((Form1.Line)).ToString();
+        }
+
+        private void txtCurrentLine_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        //double[] X = ;
+        public static double[] X = new double[36];
+        public static double[] Y = new double[36];
+        public static double[] diffXa = new double[36];
+        //double[] diffXb = new double[36];
+
+        public static double[] diffYa = new double[36];
+        //double[] diffYb = new double[36];
+
+        public void ShowLine()
+        {
+            string[] strs2 = File.ReadAllLines(strPath);
+            // 也可以指定编码方式 
+            //string[] strs2 = File.ReadAllLines(@"C:\Users\SK-ED\Desktop\123.txt", Encoding.ASCII);
+
+            int n = strs2.Length;
+
+            //清空表格中的数据
+            foreach (var series in chtLine.Series)
+            {
+                series.Points.Clear();
+
+            }
+
+            if (Form1.Line > n)
+            {
+                Form1.Line = 1;
+            }
+
+
+            int i = 0;
+            string tempA = strs2[i + Form1.Line - 1];
+
+            string[] vs = tempA.Split('T');
+            string tempD = vs[1];
+
+            string tempB = tempD.Remove(0, 8);
+            string[] tempC = tempB.Split(';');
+
+            double[] tempNumD = new double[72];
+
+            //字符串类型数组转double类型数组
+            for (int m = 0; m < tempC.Length - 1; m++)
+            {
+                tempNumD[m] = double.Parse(tempC[m]);
+
+            }
+
+            //double[] X = new double[36];
+            //double[] Y = new double[36];
+            for (int q = 0; q < 36; q++)
+            {
+
+                X[q] = tempNumD[2 * q];
+                Y[q] = tempNumD[2 * q + 1];
+            }
+
+
+
+            //double[] diffXa = new double[36];
+            //double[] diffXb = new double[36];
+
+            //double[] diffYa = new double[36];
+            //double[] diffYb = new double[36];
+
+            for (int k = 0; k < 18; k++)
+            {
+                diffXa[k] = X[k] - X[0] + k * 32.2;
+                diffXa[k + 18] = X[k + 18] - X[18] + k * 32.2;
+
+                diffYa[k] = Y[k] - Y[0];
+                diffYa[k + 18] = Y[k + 18] - Y[18];
+
+
+            }
+
+
+            double[] xData = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36 };
+
+
+            chtLine.ChartAreas[0].AxisX.Interval = 1;
+
+            //chtLine.Series.Add(i.ToString() + "-Xa");           
+            chtLine.Series[4 * i].ChartType = SeriesChartType.Line;//设置为折线
+            chtLine.Series[4 * i].Points.DataBindXY(xData, diffXa);
+            chtLine.Series[4 * i].BorderWidth = 1; //折线粗细
+            chtLine.Series[4 * i].Color = System.Drawing.Color.Red;//设置折线颜色                                         
+
+            ////chtLine.Series.Add(i.ToString() + "-Xb");
+            //chtLine.Series[4 * i + 1].ChartType = SeriesChartType.Line;
+            //chtLine.Series[4 * i + 1].Points.DataBindXY(xData, diffXb);
+            //chtLine.Series[4 * i + 1].BorderWidth = 1;
+            //chtLine.Series[4 * i + 1].Color = System.Drawing.Color.Black;//设置折线颜色                                         
+
+
+
+            //chtLine.Series.Add(i.ToString() + "-Ya");
+            chtLine.Series[4 * i + 1].ChartType = SeriesChartType.Line;
+            chtLine.Series[4 * i + 1].Points.DataBindXY(xData, diffYa);
+            chtLine.Series[4 * i + 1].BorderWidth = 1;
+            chtLine.Series[4 * i + 1].Color = System.Drawing.Color.Blue;//设置折线颜色                                         
+
+
+
+            ////chtLine.Series.Add(i.ToString() + "-Yb");
+            //chtLine.Series[4 * i + 3].ChartType = SeriesChartType.Line;
+            //chtLine.Series[4 * i + 3].Points.DataBindXY(xData, diffYb);
+            //chtLine.Series[4 * i + 3].BorderWidth = 1;
+            //chtLine.Series[4 * i + 3].Color = System.Drawing.Color.Green;//设置折线颜色                                         
+
+
+
+
+        }
+
+        private void btnLoadPath_Click(object sender, EventArgs e)
+        {
+            //SelectedPath
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "请选择*.vpp文件";
+            ofd.InitialDirectory = @"C:\Users\SK-ED\Desktop";
+            ofd.Multiselect = true;
+            ofd.Filter = "|*.txt|所有文件|*.*";
+            ofd.ShowDialog();
+            //获得我们在文件夹中所选择的全路径
+            string[] path = ofd.FileNames;
+            Form1.strPath = path[0];
+            //for (int i = 0; i < path.Length; i++)
+            //{
+            //    //将*.vpp文件的文件名加载到listbox中
+            //    cbJobName.Items.Add(Path.GetFileName(path[i]));
+
+            //    Program.SelectedPath = Path.GetFullPath(path[i]);
+            //    cbJobName.SelectedItem = Path.GetFileName(path[i]);
+            //}
+
+
+        }
+
+        private void btnToExcel_Click(object sender, EventArgs e)
+        {
+            {
+                //Excel表格存储路径
+                string file = @"C:\abc.xls";
+
+                //读取数据源文件，统计行数
+                string[] strs2 = File.ReadAllLines(strPath);
+                int n = strs2.Length;
+
+
+                Excel.Application myexcelApplication = new Excel.Application();
+                if (myexcelApplication != null)
+                {
+                    Excel.Workbook myexcelWorkbook = myexcelApplication.Workbooks.Add();
+                    //for (int j = 0; j < n; j++)
+                    //{
+
+                    Excel.Worksheet myexcelWorksheet = (Excel.Worksheet)myexcelWorkbook.Sheets.Add();
+
+
+                    //myexcelWorkbook.Sheets.Add();
+                    //Excel.Worksheet myexcelWorksheet = (Excel.Worksheet)myexcelWorkbook.Sheets[j+1];
+
+                    //
+                    myexcelWorksheet.Cells.Select();
+
+
+
+
+                        for (int i = 1; i <= 36; i++)
+                        {
+                            //设置列宽为10
+                            myexcelWorksheet.Cells[i, 1].ColumnWidth = 10;
+                            myexcelWorksheet.Cells[i, 2].ColumnWidth = 10;
+
+                            //设置文本水平居中
+                            myexcelWorksheet.Cells[i, 1].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                            myexcelWorksheet.Cells[i, 1].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                            myexcelWorksheet.Cells[i, 1].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                            myexcelWorksheet.Cells[i, 1].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+
+
+                            //设置数据格式为文本形式
+                            myexcelWorksheet.Cells[i, 1].NumberFormatLocal = "@";
+                            myexcelWorksheet.Cells[i, 2].NumberFormatLocal = "@";
+                            myexcelWorksheet.Cells[i, 4].NumberFormatLocal = "@";
+                            myexcelWorksheet.Cells[i, 5].NumberFormatLocal = "@";
+
+                            //将数据传入表格，并设置小数点位数
+                            myexcelWorksheet.Cells[i, 1] = X[i - 1].ToString("0.0000");
+                            myexcelWorksheet.Cells[i, 2] = Y[i - 1].ToString("0.0000");
+                            myexcelWorksheet.Cells[i, 4] = diffXa[i - 1].ToString("0.0000");
+                            myexcelWorksheet.Cells[i, 5] = diffYa[i - 1].ToString("0.0000");
+                        }
+
+                    //}
+
+
+
+                    //文件存在则删除
+                    if (System.IO.File.Exists(Path.GetFullPath(file)))
+                    {
+                        File.Delete(Path.GetFullPath(file));
+                    }
+
+                    //保存文件
+                    myexcelApplication.ActiveWorkbook.SaveAs(file, Excel.XlFileFormat.xlWorkbookNormal);
+
+                    myexcelWorkbook.Close();
+                    myexcelApplication.Quit();
+                }
+            }
+        }
+    }
+}
